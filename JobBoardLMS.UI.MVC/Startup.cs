85accf83a90df9619bfcf5df1dfe3134cfe1403c
tlesmeister <tlesmeister525@gmail.com>
2019-10-14 13:15:55 -0500

    Commit - Added identities, changed solution name from Solution.UI.MVC to solution name, Updated web config to reflect database

--- JobBoardLMS.UI.MVC/Startup.cs
+++ JobBoardLMS.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(JobBoardLMS.UI.MVC.Startup))]
 namespace JobBoardLMS.UI.MVC
 {
     public partial class Startup
