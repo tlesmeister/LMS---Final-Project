c5e49142d17ba24f50b9e7152d1faaf8df926be8
tlesmeister <tlesmeister525@gmail.com>
2019-10-14 15:50:40 -0500

    Commit - Made minor changes to the Contact View Model

---
+++ JobBoardLMS.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace JobBoardLMS.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Your Name is Required")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "* Your Email is Required")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "* Check Email Format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "* Subject is Required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "* Message is Required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
