933ea1e4dec7302aa90fb528461c7e1ccf951751
tlesmeister <tlesmeister525@gmail.com>
2019-10-14 20:46:30 -0500

    Commit - Contact Page is now working

--- JobBoardLMS.UI.MVC/Controllers/HomeController.cs
+++ JobBoardLMS.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
 ﻿using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using System.Net;
+using System;
+using JobBoardLMS.UI.MVC.Models;
 
 namespace JobBoardLMS.UI.MVC.Controllers
 {
@@ -22,9 +27,65 @@ namespace JobBoardLMS.UI.MVC.Controllers
         [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            //make sure the object passes validation if not return to the view with the data
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            //PASS
+            //Build the message that represents the Body of the mail message
+            string message = $"You have received an email from {cvm.Name} with a subject of {cvm.Subject}. Please respond to {cvm.Email} with your response to the following message: <br />{cvm.Message}";
+
+
+            //Create the MailMessage Object (From, To, Subject, Body) - System.Net.Mail
+            MailMessage mm = new MailMessage("postmaster@devmeister.net", "tlesmeister525@gmail.com", cvm.Subject, message);
+
+
+            //Allow for HTML in the message
+            mm.IsBodyHtml = true;
+
+
+            //Mark with High priority(This is NOT required)
+            mm.Priority = MailPriority.High;
+
+
+            //Set the ReplyToList to reply to the user NOT our email address (default behavior)
+            mm.ReplyToList.Add(cvm.Email);
+
+
+            //smtp client
+            SmtpClient client = new SmtpClient("mail.devmeister.net");
+
+
+            //Client Credentials - System.Net;
+            client.Credentials = new NetworkCredential("postmaster@devmeister.net", "Be@r8918$");
+
+
+            //try
+            try
+            {
+                //send the email
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                //code the viewbag object
+                ViewBag.CustomerMessage = $"We're sorry your request could not be complete at this time. Please try again later. Error Message: <br /> {ex.StackTrace}";
+
+                //return the form filled out
+                return View(cvm);
+            }
+
+
+            //if all goes well return the confirmation view with the object
+            return View("EmailConfirmation", cvm);
         }
     }
 }
